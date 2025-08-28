33a911f65f2a236cca2a40d97e2da78404fc4e62
Rohith Joginapally <rohithjoginapally@Mac.lan>
2025-08-28 15:49:34 -0400

    chatconfig History false

--- chatConfig.js
+++ chatConfig.js
@@ -7,7 +7,9 @@ let botOptions=chatConfig.botOptions;
  botOptions.botInfo = { name: "Southwire’s CableTechSupport™ Services’ Re3™ Chatbot", "_id": "st-4ad03619-ea5d-5f8d-b807-ec12c8e9032d" };
  botOptions.clientId = "cs-12455619-79ad-5225-8841-02b6e722dbd4";
  botOptions.clientSecret = "IYNA709fsWr1UsjvDr40T6X9Sy1JNQ2UUkxNEQaxSCI=";
- botOptions.loadHistory =  false;
+//  botOptions.loadHistory =  false;
+
+ chatConfig.history.enable = false;
 
 var chatWindowInstance = new chatWindow(chatConfig);
 chatWindowInstance.show(chatConfig);
