bb22a1095a8bc395cacfdbe4066c9a5293972110
Rohith Joginapally <rohithjoginapally@Mac.lan>
2025-09-02 15:46:56 -0400

    My Own JWT

--- chatConfig.js
+++ chatConfig.js
@@ -7,7 +7,8 @@ let botOptions=chatConfig.botOptions;
 userIdentity = crypto.randomUUID();
 // KoreChatSDK.chatConfig.botOptions.userIdentity = userIdentity;
 	
- botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
+//  botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
+botOptions.JWTUrl = "https://jwt-service-2xuo.onrender.com/sts";
  botOptions.userIdentity = userIdentity;
  botOptions.botInfo = { name: "Southwire’s CableTechSupport™ Services’ Re3™ Chatbot", "_id": "st-4ad03619-ea5d-5f8d-b807-ec12c8e9032d" };
  botOptions.clientId = "cs-12455619-79ad-5225-8841-02b6e722dbd4";
