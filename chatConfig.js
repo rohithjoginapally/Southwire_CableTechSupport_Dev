7675142455552cf4373d0c98dcb915a09a6712c6
Rohith Joginapally <rohithjoginapally@Mac.lan>
2025-09-02 20:59:26 -0400

    vercel App JWT

--- chatConfig.js
+++ chatConfig.js
@@ -8,7 +8,8 @@ userIdentity = crypto.randomUUID();
 // KoreChatSDK.chatConfig.botOptions.userIdentity = userIdentity;
 	
 //  botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
-botOptions.JWTUrl = "https://jwt-service-1.onrender.com/sts";
+// botOptions.JWTUrl = "https://jwt-service-1.onrender.com/sts";
+botOptions.JWTUrl = "https://kore-sts-vercel.vercel.app/api/sts";
  botOptions.userIdentity = userIdentity;
  botOptions.botInfo = { name: "Southwire’s CableTechSupport™ Services’ Re3™ Chatbot", "_id": "st-4ad03619-ea5d-5f8d-b807-ec12c8e9032d" };
  botOptions.clientId = "cs-12455619-79ad-5225-8841-02b6e722dbd4";
