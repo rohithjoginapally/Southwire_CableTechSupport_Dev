5c1d1ee5b6671e47dfe38fd509897171cc599e7c
Rohith Joginapally <rohithjoginapally@Mac.lan>
2025-09-03 11:33:18 -0400

    New CDN and Cleaned JS

--- chatConfig.js
+++ chatConfig.js
@@ -1,19 +1,13 @@
-// import { chatConfig, chatWindow } from 'kore-web-sdk';
-
 var chatConfig = KoreChatSDK.chatConfig;
 var chatWindow = KoreChatSDK.chatWindow;
 let botOptions=chatConfig.botOptions;
 
 userIdentity = crypto.randomUUID();
-// KoreChatSDK.chatConfig.botOptions.userIdentity = userIdentity;
-	
-//  botOptions.JWTUrl = "https://mk2r2rmj21.execute-api.us-east-1.amazonaws.com/dev/users/sts";
-// botOptions.JWTUrl = "https://jwt-service-1.onrender.com/sts";
+
 botOptions.JWTUrl = "https://kore-sts-vercel.vercel.app/api/sts";
  botOptions.userIdentity = userIdentity;
  botOptions.botInfo = { name: "Southwire’s CableTechSupport™ Services’ Re3™ Chatbot", "_id": "st-4ad03619-ea5d-5f8d-b807-ec12c8e9032d" };
  botOptions.clientId = "cs-12455619-79ad-5225-8841-02b6e722dbd4";
-//  botOptions.clientSecret = ";
 
  chatConfig.history.enable = false;
 var chatWindowInstance = new chatWindow(chatConfig);
